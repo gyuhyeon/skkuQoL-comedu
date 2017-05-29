d96759ca22fade28b77c62b8e0b9decb7d2718d3
Gyuhyeon_Lee <mark3236@gmail.com>
2017-05-29 20:38:45 +0900

    PHP tutorial

---
+++ QOLassets/PHPtutorial.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+      <title>Hello World!</title>
+      <link rel="STYLESHEET" type="text/css" href="contact.css">
+</head>
+<body>
+    <h1> Hello! </h1>
+    <ul>
+    <li>
+    <?php
+    //start of php code
+    $sometext="Hello World whose time was ".time();
+    ?>
+    </li>
+    </ul>
+</body>
+</html>
