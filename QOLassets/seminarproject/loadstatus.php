b935c3bcd5784c59d4e75629e8609f2e4e281cba
Gyuhyeon_Lee <mark3236@gmail.com>
2017-05-30 01:55:11 +0900

    php file added for db connection test. Caution : remember not to expose sensitive password in publicly commited files.

---
+++ QOLassets/seminarproject/loadstatus.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "insecurelocalpassword"; //root assigned for only localhost connection uses insecure password for github exposure.
+$dbname = "admin";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT user_id FROM admin.xe_member";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["user_id"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
