ed0ca8d7e0b3bb87ff5e15091c9b2bdfd81e0415
Gyuhyeon Lee <mark3236@naver.com>
2017-05-30 19:01:46 +0900

    changed explicit day into D1 ~ D7. Need to load from database… This will probably break everything for now.

--- QOLassets/seminarproject/reserve.js
+++ QOLassets/seminarproject/reserve.js
@@ -9,24 +9,6 @@ function updateTableColor() {
 	var d = document.getElementsByName("day")[0];
 	var st = document.getElementsByName("start_time")[0];
 	var et = document.getElementsByName("end_time")[0];
-	var datedict = {
-		"mon": "월",
-		"tue": "화",
-		"wed": "수",
-		"thu": "목",
-		"fri": "금",
-		"sat": "토",
-		"sun": "일"
-	};
-	var reversedatedict = {
-		"월": "mon",
-		"화": "tue",
-		"수": "wed",
-		"목": "thu",
-		"금": "fri",
-		"토": "sat",
-		"일": "sun"
-	};
 
 	//change background for cells with reservation
 	var td = document.getElementById('schedule').getElementsByTagName('td');
@@ -40,11 +22,11 @@ function updateTableColor() {
 	//change background for selected cells
 	if (d.value != "선택") {
 		if (st.value != "선택") {
-			$("[name='col_" + reversedatedict[d.value] + "'] > [name='" + st.value.slice(0, 2) + "']")[0].style.backgroundColor = selectedColor;
+			$("[name='" + d.value + "'] > [name='" + st.value.slice(0, 2) + "']")[0].style.backgroundColor = selectedColor;
 		}
 		if (st.value != "선택" && et.value != "선택") {
 			for (var i = parseInt(st.value.slice(0, 2)); i <= parseInt(et.value.slice(0, 2)); ++i) {
-				$("[name='col_" + reversedatedict[d.value] + "'] > [name='" + i + "']")[0].style.backgroundColor = selectedColor;
+				$("[name='" + d.value + "'] > [name='" + i + "']")[0].style.backgroundColor = selectedColor;
 			}
 		}
 	}
@@ -64,18 +46,9 @@ function clickToReserve(day, time) {
 	var st = document.getElementsByName("start_time")[0];
 	var et = document.getElementsByName("end_time")[0];
 
-	var datedict = {
-		"mon": "월",
-		"tue": "화",
-		"wed": "수",
-		"thu": "목",
-		"fri": "금",
-		"sat": "토",
-		"sun": "일"
-	};
 
 	//refuse if the time is unavailable
-	if ($("[name='col_" + day + "'] > [name='" + time + "']")[0].innerText != "") {
+	if ($("[name='" + day + "'] > [name='" + time + "']")[0].innerText != "") {
 		alert("예약할 수 없는 시간입니다 :(");
 	}
 	// case when the click needs to register as start time
@@ -84,8 +57,8 @@ function clickToReserve(day, time) {
 	// B. Selection date has changed
 	// C. start_time is bigger than time(parameter passed to current function) to register
 	// D. end_time is already set(improve user experience by resetting)
-	else if (d.value == "선택" || st.value == "선택" || datedict[day] != d.value || parseInt(st.value.slice(0, 2)) > time || et.value != "선택") {
-		d.value = datedict[day]; // set date(요일)
+	else if (d.value == "선택" || st.value == "선택" || day != d.value || parseInt(st.value.slice(0, 2)) > time || et.value != "선택") {
+		$("[name='day'] > ." + day)[0].selected=true; // set date(요일)
 		st.value = time + ":00"; // set time(start time)
 		et.value = "선택"; // reset endtime selection for the user
 	}
@@ -101,43 +74,26 @@ function validateSelection() {
 	var d = document.getElementsByName("day")[0];
 	var st = document.getElementsByName("start_time")[0];
 	var et = document.getElementsByName("end_time")[0];
-	var datedict = {
-		"mon": "월",
-		"tue": "화",
-		"wed": "수",
-		"thu": "목",
-		"fri": "금",
-		"sat": "토",
-		"sun": "일"
-	};
-	var reversedatedict = {
-		"월": "mon",
-		"화": "tue",
-		"수": "wed",
-		"목": "thu",
-		"금": "fri",
-		"토": "sat",
-		"일": "sun"
-	};
+
 	//endtime is earlier than starttime
 	if (st.value != "선택" && et.value != "선택" && parseInt(st.value.slice(0, 2)) > parseInt(et.value.slice(0, 2))) {
 		et.value = "선택";
 		alert("시작시간 이후의 종료시간을 선택해주세요 :(");
 	}
 	//day is set, and starttime is occupied
-	else if (d.value != "선택" && st.value != "선택" && parseInt(st.value.slice(0, 2)) && $("[name='col_" + reversedatedict[d.value] + "'] > [name='" + st.value.slice(0, 2) + "']")[0].innerText != "") {
+	else if (d.value != "선택" && st.value != "선택" && parseInt(st.value.slice(0, 2)) && $("[name='" + d.value] + "'] > [name='" + st.value.slice(0, 2) + "']")[0].innerText != "") {
 		st.value = "선택";
 		alert("해당 시작시간은 이미 예약되어 있습니다 :(");
 	}
 	//day is set, and endtime is occupied
-	else if (d.value != "선택" && et.value != "선택" && parseInt(st.value.slice(0, 2)) && $("[name='col_" + reversedatedict[d.value] + "'] > [name='" + et.value.slice(0, 2) + "']")[0].innerText != "") {
+	else if (d.value != "선택" && et.value != "선택" && parseInt(st.value.slice(0, 2)) && $("[name='" + d.value] + "'] > [name='" + et.value.slice(0, 2) + "']")[0].innerText != "") {
 		et.value = "선택";
 		alert("해당 시작시간은 이미 예약되어 있습니다 :(");
 	}
 	//day is set, both starttime and endtime is specified, which means we can safely loop within it. check if occupied.
 	else if (d.value != "선택" && st.value != "선택" && et.value != "선택") {
 		for (var i = parseInt(st.value.slice(0, 2)); i <= parseInt(et.value.slice(0, 2)); ++i) {
-			if ($("[name='col_" + reversedatedict[d.value] + "'] > [name='" + i + "']")[0].innerText != "") {
+			if ($("[name='" + d.value + "'] > [name='" + i + "']")[0].innerText != "") {
 				et.value = "선택";
 				alert("중간에 예약되어 있는 시간이 있습니다 :(");
 			}
