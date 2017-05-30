d5b8c41af9aa3b48248adffcc819a5934ce37d47
Gyuhyeon Lee <mark3236@naver.com>
2017-05-30 17:54:28 +0900

    fixed typos

--- QOLassets/seminarproject/reserve.js
+++ QOLassets/seminarproject/reserve.js
@@ -202,23 +202,22 @@ $(document).ready(function() {
 
 
 function updateTableData(){
-	$.ajax({
 		// process the form
-        $.ajax({
-            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-            url         : 'process.php', // the url where we want to POST
-            data        : formData, // our data object
-            dataType    : 'json', // what type of data do we expect back from the server
+    $.ajax({
+        type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+        url         : 'process.php', // the url where we want to POST
+        data        : formData, // our data object
+        dataType    : 'json', // what type of data do we expect back from the server
                         encode          : true
-        })
+    })
             // using the done promise callback
-            .done(function(data) {
+        .done(function(data) {
 
-                // log data to the console so we can see
-                console.log(data); 
+            // log data to the console so we can see
+            console.log(data); 
 
-                // here we will handle errors and validation messages
-            });
+            // here we will handle errors and validation messages
+        });
 
 	})
 }
