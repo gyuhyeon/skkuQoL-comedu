d68ea5519204f1347d765e881443ea4d8dc22a39
Gyuhyeon Lee <mark3236@naver.com>
2017-06-04 07:31:05 +0900

    easy safe db query page

---
+++ QOLassets/seminarproject/select.php
@@ -0,0 +1,64 @@
+<?php
+$dbservername = "localhost";
+$dbusername = "root";
+$dbpassword = "insecurelocalpassword";
+$dbname = "admin";
+
+// Create connection
+$conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//set characterset that php thinks our database is using
+if (!$conn->set_charset("utf8")) {
+	die("utf8 문자 세트를 가져오다가 에러가 났습니다 :".$conn->error." 현재 문자 세트 : ".$conn->character_set_name());
+}
+
+$currentdate = $_POST['currentdate'];
+//set response header
+header('Content-type:application/json;charset=utf-8');
+$response='NULL';
+
+//find all reservations til 6 days from now
+$sql = "SELECT * FROM admin.qol_seminarreservelist WHERE (reservedate >= '$currentdate' and reservedate <= DATE(DATE_ADD($currentdate, INTERVAL 6 DAY)))";
+
+//run mysql query
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    echo "<table>";
+    echo "<tr>";
+    echo "<td>reserve_srl</td>";
+    echo "<td>purpose</td>";
+    echo "<td>reservedate</td>";
+    echo "<td>starttime</td>";
+    echo "<td>endtime</td>";
+    echo "<td>groupname</td>";
+    echo "<td>studentname</td>";
+    echo "<td>password</td>";
+    echo "</tr>";
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>".$row["reserve_srl"]."</td>";
+        echo "<td>".$row["purpose"]."</td>";
+        echo "<td>".$row["reservedate"]."</td>";
+        echo "<td>".$row["startime"]."</td>";
+        echo "<td>".$row["endtime"]."</td>";
+        echo "<td>".$row["groupname"]."</td>";
+        echo "<td>".$row["studentname"]."</td>";
+        echo "<td>".$row["password"]."</td>";
+        echo "</tr>";
+        
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+echo $response;
+
+//close connection
+$conn->close();
+?>
