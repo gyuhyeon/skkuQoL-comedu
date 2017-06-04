5b12813ff87d8d4dea9f1676398895d870cf3491
Gyuhyeon Lee <mark3236@naver.com>
2017-06-05 00:39:41 +0900

    aAAaaa

--- QOLassets/proxy.php
+++ QOLassets/proxy.php
@@ -2,19 +2,21 @@
 $ch = curl_init();
 //here's your fucking header... you wasted 4 hours of my life.
 $header = array(
-   'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0.2) Gecko/20100101 Firefox/6.0.2',
-    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language: en-gb,en;q=0.5',
-    'Accept-Encoding: gzip, deflate',
+   'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language: en-US,en;q=0.8,ko;q=0.6',
     'Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7',
     'Proxy-Connection: Close',
-    'Cookie: PREF=ID=2bb051bfbf00e95b:U=c0bb6046a0ce0334:',
+    'Cookie: 	__utmt=1; __utma=12798129.1253298825.1496590349.1496590349.1496590349.1; __utmb=12798129.1.10.1496590349; __utmc=12798129; __utmz=12798129.1496590349.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)',
     'Cache-Control: max-age=0',
     'Connection: Close'
 );
+$agent;
 // set URL and other appropriate options
 curl_setopt($ch, CURLOPT_URL, "http://coe.skku.edu/coe/menu_6/data_01.jsp");
 curl_setopt($ch, CURLOPT_HEADER, $header);
+curl_setopt($ch, CURLOPT_REFERER, 'http://www.google.com');
+curl_setopt($ch, CURLOPT_AUTOREFERER, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 // grab URL and pass it to the browser
