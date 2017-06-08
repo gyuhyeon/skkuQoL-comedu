2cac65d458108bab59582b5fd60903598d1bf088
Gyuhyeon Lee <mark3236@naver.com>
2017-06-09 00:58:58 +0900

    added additional security(but insecure in reality)

---
+++ qol/seminarproject/secretlink.php
@@ -0,0 +1,33 @@
+﻿<?php
+//set response header
+header('Content-type:application/json;charset=utf-8');
+$response='NULL';
+
+if(!isset($_POST['loggedmembersrl'])){
+    $response="로그인한 관리자만 열람 가능합니다.";
+    echo json_encode(["response" => $response]);    
+    die();
+}
+$loggedmembersrl = $_POST['loggedmembersrl'];
+//insecure. this information should be stored elsewhere outside of root.
+$allowed = array(147, 987);
+$auth=FALSE;
+for($i=0; $i<count($allowed); ++$i){
+    if($loggedmembersrl==$allowed[$i]){
+        $auth=TRUE;
+    }
+}
+if($auth===FALSE){
+    $response="관리자만 열람 가능합니다.";
+    echo json_encode(["response" => $response]);    
+    die();
+}
+else{
+    $response="success";
+    //also insecure
+    $link="http://comedu.co.kr/qol/seminarproject/reserve_status.html";
+    echo json_encode(["response" => $response, "link" => $link]);
+    die();
+}
+
+?>
